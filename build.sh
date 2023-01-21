8ae821f32599d96068ef65ef731812fda45d03ba
Tino <58232152+tinoha@users.noreply.github.com>
2023-01-21 21:29:08 +0200

    add build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+podman build --cap-add=DAC_READ_SEARCH,SETGID,SETUID,NET_BIND_SERVICE \
+  --format docker \
+  -t docker.io/tihal/omada-controller:5.7.4 \
+  -f Dockerfile \
+  --label org.opencontainers.image.authors="Tino <tinoha10@outlook.com>" \
+  --label org.opencontainers.image.url="https://github.com/tinoha/omada-controller" \
+  --label org.opencontainers.image.documentation="https://github.com/tinoha/omada-controller/blob/main/README.md" \
+  --label org.opencontainers.image.version="5.7.4" \
+  --label org.opencontainers.image.created="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
+  --label org.opencontainers.image.revision="$(git log main -1 --format=%h)" \
+
